45a27387bcfab3e0abcb7c64ba8d8610daf55399
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2022-08-21 22:03:52 +0800

    Second Stage bugfix

--- script/Second_Stage/Keyboard_Subscriber.py
+++ script/Second_Stage/Keyboard_Subscriber.py
@@ -13,10 +13,8 @@ if __name__ == '__main__':
     rospy.init_node("keyboard_subscriber")
     pub = rospy.Subscriber('keyboard_topic', String, callback)
 
-    # 初始化Serial
+    # Init Serial
     port = rospy.get_param('/port')
     baudrate = rospy.get_param('/baudrate')
-    print(port, baudrate)
-
-    
-
+    ser = serial.Serial(port, baudrate)
+    rospy.spin()
