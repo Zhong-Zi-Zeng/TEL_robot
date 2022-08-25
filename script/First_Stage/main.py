327949313c010192f47b629023079928b2e13cf5
Zhong-Zi-Zeng <ximen0806449@gmail.com>
2022-08-25 17:34:45 +0800

    Test maim.py. Add first_stage parameter.yaml

--- script/First_Stage/main.py
+++ script/First_Stage/main.py
@@ -1,17 +1,35 @@
-import cv2
-import sys
+#!/usr/bin/env python
 from Yolo_V4_Api import Detect
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+import cv2
+import rospy
 
 
-network = Detect()
+# =====影像回調函式=====
+def img_callback(img_msgs):
+    img = bridge.imgmsg_to_cv2(img_msgs, 'bgr8')
+    detections = network.detect_image(img)
 
-cap = cv2.VideoCapture(0)
+    print(detections)
 
-
-while True:
-    ret, img = cap.read()
-
-    detect = network.detect_image(img)
-
-    print(detect)
+    cv2.imshow('', img)
     cv2.waitKey(1)
+
+
+
+
+if __name__ == '__main__':
+    network = Detect()
+
+    # 訂閱影像節點
+    bridge = CvBridge()
+    subscriber = rospy.Subscriber('/camera/color/image_raw', Image, img_callback)
+    rospy.spin()
+
+
+
+
+
+
+
